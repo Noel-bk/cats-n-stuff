ee77b866cdf42e3d65b139a2fcb80ab020f5d89c
Noel.b <jaeyeong.b@gmail.com>
2017-04-09 22:46:34 +0900

    Add React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from './Jumbotron';
 
 class App extends Component {
   render() {
@@ -13,7 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Hello World!</h1>
+        <Jumbotron />
       </div>
     );
   }
